533f532f8370a1614e80765a80975abc77a5d1b5
Augustine Normanyo <augustinenormanyo98@gmail.com>
2025-11-15 18:59:31 +0000

    feat: created an event page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventBanner from '@/components/event/eventBanner'
+import ImageDisplay from '@/components/event/imageDisplay'
+
+export default function page() {
+  return (
+    <div>
+        <EventBanner />
+        <ImageDisplay />
+    </div>
+  )
+}
